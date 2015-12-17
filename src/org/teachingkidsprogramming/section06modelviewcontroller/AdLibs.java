da1b50fe0d4e84e1abb07d34bca8372c72796abf
TheDailonY <91104200@PLMS-LAP-ST12.mvusd.k12.ca.us>
2015-12-17 14:49:13 -0800

    A few changes

--- src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -4,23 +4,70 @@ import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
-  public static void main(String[] args)
-  {
+	public static int badInputCounter = 0;
+  
+	public static void main(String[] args)
+  {		
+		//int answer = 90;
+		
     String numberOfPeopleYouKilled = MessageBox.askForTextInput("Tell how many people you killed.");
-    String didAfterWards = MessageBox.askForTextInput("What did you do next.");
     if (numberOfPeopleYouKilled.isEmpty())
     {
+    	badInputCounter++;
       MessageBox.showMessage("Follows instructions or leave.");
-      return;
+      numberOfPeopleYouKilled = askNumberOfPeopleYouKilledAgain();
     }
-    else if (numberOfPeopleYouKilled.matches("[\\d]*"))
-    {
-      MessageBox.showMessage("That is not an adverb get it right.");
-      return;
-    }
+    String didAfterWards = MessageBox.askForTextInput("What did you do next.");
+   if(didAfterWards.isEmpty())
+   {
+	   badInputCounter++;
+	   MessageBox.showMessage("Follows instructions or leave.");
+	      didAfterWards = askDidAfterWardsAgain();
+   }
+   if(didAfterWards.matches("[\\d]*")) 
+   {
+	   badInputCounter++;
+	   if(badInputCounter >= 3)
+	   {
+		   MessageBox.showMessage("I told you you were going to leave!!!");
+		   System.exit(0);
+	   }
+	MessageBox.showMessage("That is not what you did afterwards. You could try again.");
+	didAfterWards = askDidAfterWardsAgain();
+   }
     String currentBodyPart = MessageBox.askForTextInput("Enter a body part.");
+    if(currentBodyPart.isEmpty()) 
+    {
+    	badInputCounter++;
+    	 MessageBox.showMessage("Follows instructions or leave.");
+         currentBodyPart = askCurrentBodyPartAgain();
+    }
+    if(currentBodyPart.matches("[\\d]*")) 
+    {
+    	badInputCounter++;
+ 	MessageBox.showMessage("That is not a body part. You could try again.");
+ 	currentBodyPart = askCurrentBodyPartAgain();
+    }
     String VeteranStory = (" Today I woke up and killed " + numberOfPeopleYouKilled + " people, " + "then I "
         + didAfterWards + " ." + "Then I chopped this one guy's " + currentBodyPart + " off.");
     MessageBox.showMessage(VeteranStory);
+  
+  }//end of main
+  
+  public static String askNumberOfPeopleYouKilledAgain() 
+  {
+	  String numberOfPeopleYouKilled = MessageBox.askForTextInput("Tell how many people you killed.");
+	  return numberOfPeopleYouKilled;
   }
-}
+  public static String askDidAfterWardsAgain() 
+  {
+	  String didAfterWards = MessageBox.askForTextInput("What did you do next.");
+	  return didAfterWards;
+  }
+  public static String askCurrentBodyPartAgain() 
+  {
+	  String currentBodyPart = MessageBox.askForTextInput("Enter a body part.");
+	  return currentBodyPart;
+  }
+   
+}//end of Adlibs class
---
+++ src/org/teachingkidsprogramming/section06modelviewcontroller/DebuggerPractice.java
@@ -0,0 +1,17 @@
+package org.teachingkidsprogramming.section06modelviewcontroller;
+
+public class DebuggerPractice 
+{
+	@SuppressWarnings("unused")
+	public static void main(String[] args) 
+	{
+		int myNumber = 22;
+		String myString = "Im stringin it!";
+		float MYfloat = 2.3f;
+		double myDouble = 2.3;
+		boolean myBoolean = false;
+		Integer myRefInt = 11;
+		Double myRefDouble = 2.42323;
+System.out.println("This is the end of the main method.");
+	}
+}
